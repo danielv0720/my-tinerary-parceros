11c90b5be3527cb229939d8c369c776d855745b8
Alejandro Velez <alejovelez@nebular.co>
2022-11-05 18:05:35 -0500

    Creacion componente carousel,flechas,footer,home2,scroltotop

---
+++ src/components/Scrolltotop/Scrolltotop.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function Scrolltotop() {
+
+  const [scrolltotop,setScrolltotop] = useState(false)
+
+
+useEffect(()=>{
+  window.addEventListener("scroll",()=>{
+    if(window.scrollY > 100){
+      setScrolltotop(true)
+    }else{
+      setScrolltotop(false)
+    }
+  })
+},[])
+
+const scrollUp=() =>{
+  window.scrollTo({
+    top:0,
+    behavior: "smooth"
+  })
+}
+
+  return (
+  <>
+    {scrolltotop &&(
+      <button style={{
+        position: "fixed",
+        bottom: "50px",
+        right: "50px",
+        height: "50px",
+        width:"50px",
+        fontSize:"50px"
+      }}
+      onClick={scrollUp}>^</button>
+    )}
+</>
+    )
+    }
+    
+
+
---
+++ src/data2/hotelsData.js
@@ -0,0 +1,111 @@
+let hotelsData=[
+  {
+    id:"ho1",
+    name:"Esplendor by Wyndham Savoy",
+    photo:["https://www.wyndhamhotels.com/content/dam/property-images/en-us/es/ar/others/rosario/51076/51076_exterior_view_2.jpg","https://cf.bstatic.com/xdata/images/hotel/max500/174293883.jpg?k=81163e3f9127084aa45dae2006c29de5da751a3eb2702943212aa2c1ee18651d&o=&hp=1","https://esplendorsavoyrosario.com/wp-content/uploads/sites/6/2020/08/hotel-esplendor-rosario-home-superior.jpg"],
+    capacity:225,
+    description:" Built in 1910, this emblematic building was recently renovated and is located in the heart of the city of Rosario. It has 75 Concept rooms, Concept Superior, VIP Suites and Luxury.Restaurant & Bar, terrace.",
+    citiId:"city1",
+    userId:"admin1"
+  },
+  {
+    id:"ho2",
+    name:"Mine Hotel Boutique",
+    photo:["https://media-cdn.tripadvisor.com/media/photo-s/0b/3e/e2/42/garden-area.jpg","https://pix10.agoda.net/hotelImages/6771350/0/9bebf6e7844c0dc19083eca75214d716.jpg?ca=8&ce=1&s=1024x768","https://www.kayak.com.co/rimg/himg/d4/53/65/expediav2-186916-5a49f3-681964.jpg?width=1366&height=768&crop=true"],
+    capacity:310,
+    description:" Mine Hotel Boutique is an excellent choice for travelers visiting Buenos Aires, offering a romantic environment alongside many helpful amenities designed to enhance your stay.",
+    citiId:"city2",
+    userId:"admin1"
+  },
+  {
+    id:"ho3",
+    name:"Rio Othon Palace Hotel",
+    photo:["https://www.letsbookhotel.com/img/max300/759/7590616.jpg","https://www.arserver.info/img/hotels/179/big_hotel-rio-othon-palace-01.jpg","https://cf.bstatic.com/xdata/images/hotel/max300/290904715.jpg?k=2157e322e872fc357cdf71612a1b4bcd40f306d4bcad1d8604b88172de3da127&o=&hp=1"],
+    capacity:410,
+    description:" For those who are interested in visiting well-known landmarks during their trip to Rio de Janeiro, Rio Othon Palace Hotel is close to Leme (2.9 km) and Forte Duque de Caxias (3.0 km).",
+    citiId:"city3",
+    userId:"admin1"
+  },
+  {
+    id:"ho4",
+    name:"Pullman Sao Paulo Ibirapuera",
+    photo:["https://images.squarespace-cdn.com/content/v1/60d9c242b704170d76d4eb33/1625172929442-GTJPNHBCG8F4NMFOAVK4/Banner+Home+-+Fachada+Pullman+Ibirapuera.jpg?format=2500w","https://images.squarespace-cdn.com/content/v1/60d9c242b704170d76d4eb33/1629126568419-1URPW963URH788JJHVMI/Pullman+Ibirapuera+-+Lobby+e+Globo+Pullman.jpg","https://media.hrs.com/media/image/bc/34/b9/Pullman_Sao_Paulo_Ibirapuera-Sao_Paulo-Aussenansicht-6-77482.jpg"],
+    capacity:390,
+    description:" Pullman Sao Paulo Ibirapuera is an excellent choice for travelers visiting Sao Paulo, offering a trendy environment alongside many helpful amenities designed to enhance your stay..",
+    citiId:"city4",
+    userId:"admin1"
+  },
+  {
+    id:"ho5",
+    name:"Hotel South Beach",
+    photo:["https://cdn.kiwicollection.com/media/property/PR011080/xl/011080-01-rooftop%20pool%20at%201%20Hotel%20South%20Beach.jpg?cb=1595871402","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/92/62/5a/sbh-south-beach-hotel.jpg?w=700&h=-1&s=1","https://www.kayak.com.co/rimg/himg/39/77/62/leonardo-95901502-5_Rooms-2BEDS_ocean_view_O-935310.jpg?width=1366&height=768&crop=true"],
+    capacity:1200,
+    description:" Hotel South Beach is an excellent choice for travelers visiting Miami Beach, offering a romantic environment alongside many helpful amenities designed to enhance your stay.",
+    citiId:"city5",
+    userId:"admin1"
+  },
+  {
+    id:"ho6",
+    name:"Mr. C Beverly Hills",
+    photo:["https://static-new.lhw.com/HotelImages/Final/LW2750/lw2750_123541027_790x490.jpg","https://images.squarespace-cdn.com/content/v1/526a41a5e4b015cba634554b/1394646751226-0Y16483HECBHWBXMAIC4/MrCBeverlyHills-LA+Pool.jpg?format=1000w","https://cdn.kiwicollection.com/media/room_images/PR009237/xl/009237-c-suitebedroom-v4s.jpg"],
+    capacity:170,
+    description:" For those interested in checking out popular landmarks while in Los Angeles, the Mr. C Beverly Hills Hotel is close to Little Ethiopia (2 miles) and Los Angeles California Temple, The Church of Jesus Christ of Latter-Day Saints. (3.3 km).",
+    citiId:"city6",
+    userId:"admin1"
+  },
+  {
+  id:"ho7",
+  name:" JW Marriott Marquis",
+  photo:["https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/3f/1c/77/exterior.jpg?w=700&h=-1&s=1","https://www.oyster.com/wp-content/uploads/sites/35/2019/05/pool-v16976953-1440.jpg","http://secure.s.forbestravelguide.com/img/properties/jw-marriott-marquis-hotel-dubai/jw-marriott-marquis-hotel-dubai-presidential-suite-bedroom.jpg"],
+  capacity:530,
+  description:" JW Marriott Marquis Hotel Dubai is an excellent choice for travelers visiting Dubai, offering a luxurious environment alongside many helpful amenities designed to enhance your stay.",
+  citiId:"city7",
+  userId:"admin1"
+},
+{
+  id:"ho8",
+  name:" voco Orchard",
+  photo:["https://static.wixstatic.com/media/8471ce_53edf95395464ae4a1b8a6723e76b85a~mv2.png/v1/fit/w_1000%2Ch_1000%2Cal_c/file.png","https://pix10.agoda.net/hotelImages/10596/-1/ff40cd9aef8f59a31957294d913ac7a7.jpg?ca=9&ce=1&s=1024x768","https://milelion-com0.exactdn.com/wp-content/uploads/2021/09/photo_2021-09-30_17-40-21.jpg?strip=all&lossy=1&ssl=1"],
+  capacity:720,
+  description:" In Singapore there are a large number of Chinese food restaurants. So when youre here, dont miss places like Palm Beach Seafood Restaurant, JUMBO Seafood (Riverside Point).",
+  citiId:"city8",
+  userId:"admin1"
+},
+{
+  id:"ho9",
+  name:" The Montcalm Royal London House",
+  photo:["https://i.travelapi.com/hotels/13000000/12710000/12703900/12703815/31d17948_z.jpg","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/86/5e/5d/montcalm-royal-london.jpg?w=700&h=-1&s=1","https://www.themontcalm.com/wp-content/themes/themontcalm/images/hotel_images/the_montcalm_royal_london_house/new/RLH-hotel-landing-04.jpg"],
+  capacity:430,
+  description:" See why so many travelers make Montcalm Royal London House - City of London their hotel of choice when visiting London. In addition to providing the ideal combination of quality, comfort and location.",
+  citiId:"city9",
+  userId:"admin1"
+},
+{
+  id:"ho10",
+  name:" Hotel de Berri Champs-Élysées",
+  photo:["https://www.privateupgrades.com/_data/default-hotel_image/11/55319/hotel-de-berri-3_1400x1400_auto.jpg","https://d17k8relgo9zj1.cloudfront.net/rcimg/2/9kzIHJayFJV7DqE7o9Pshh6zFfk1Gy9UHHnXhufaqFo/fit/0/1000/ce/1/aHR0cHM6Ly9pLnRyYXZlbGFwaS5jb20vaG90ZWxzLzEwMDAwMDAvODgwMDAwLzg3NDIwMC84NzQxNzgvNWMyMzFlZWIuanBn.jpg","https://i.ytimg.com/vi/irJhif3zmBg/maxresdefault.jpg"],
+  capacity:510,
+  description:" Hotel de Berri, A Luxury Collection Hotel, Paris puts the best of Paris at your fingertips, making your stay both relaxing and enjoyable.",
+  citiId:"city10",
+  userId:"admin1"
+},
+{
+  id:"ho11",
+  name:" Oakwood Premier",
+  photo:["https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/45/07/e8/oakwood-premier-phnom.jpg?w=700&h=-1&s=1","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/c5/79/58/swimming-pool.jpg?w=700&h=-1&s=1","https://cf.bstatic.com/xdata/images/hotel/max300/156924928.jpg?k=8b228725d14ecd1554c7498660c7eb752eb75cb3a1cfb1bdea05298883072f01&o=&hp=1"],
+  capacity:470,
+  description:" The rooms at the hotel offer a flat screen TV, a refrigerator, and a kitchenette to make you feel at home, and getting online is easy, with free wifi available..",
+  citiId:"city11",
+  userId:"admin1"
+},
+{
+  id:"ho12",
+  name:" Mövenpick Moscow Taganskaya",
+  photo:["https://ak-d.tripcdn.com/images/0201g1200089esrh7A8A9_R_800_525.jpg","https://cf.bstatic.com/xdata/images/hotel/max1024x768/273817353.jpg?k=58585b5635b0f489b0571ceb8924fa462ca5aee18b84442d3aa24aab33dba7ce&o=&hp=1","https://m.ahstatic.com/is/image/accorhotels/Moscow_Taganskaya__i129196:3by2?wid=546&hei=365&resMode=sharp2&op_usm=1.75,0.3,2,0&dpr=off&iccEmbed=true&icc=sRGB&qlt=65"],
+  capacity:620,
+  description:" An excellent choice for travelers visiting Wellington, offering a romantic environment alongside many helpful amenities designed to enhance your stay.",
+  citiId:"city12",
+  userId:"admin1"
+}
+]
+export default hotelsData
