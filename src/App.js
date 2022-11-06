94475b72f0373f33391d1e9332535fd4f4c01cd1
Alejandro Velez <alejovelez@nebular.co>
2022-11-06 15:00:58 -0500

    page not found

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // Components
 
 import Home1 from './pages/Home1'
+import Home2 from './components/Home2/Home2.jsx'
 // Routes
 import { Routes, Route } from 'react-router-dom';
 // Styles
 import './App.css';
 import './components/NotFound/NotFound.css'
+// import './components/Signin/Signin.css'
 // Scroll To Top when change route
 import AutoToTop from './components/AutoToTop';
 // Components
@@ -13,6 +15,7 @@ import Cities from './pages/Cities';
 import Hotels from './pages/Hotels';
 import Layout from './layout/Layout';
 import NotFoundPage from './pages/NotFoundPage';
+// import SigninPage from './pages/SigninPage';
 
 
 // Layout
@@ -26,9 +29,12 @@ function App() {
       <AutoToTop />
       <Routes>
         <Route path='/' exact element={<Home1 />}/>
+        <Route path='/' exact element={<Home2 />}/>
         <Route path='/cities' element={<Cities />}/>
         <Route path='/hotels' element={<Hotels />}/>
         <Route path='*' element={<NotFoundPage />}/>
+        {/* <Route path='/Signin' element={<SigninPage/>}/> */}
+        
       </Routes>
     </Layout>
     
