3d8750ade3192c185845bd1f510d1cb2a1b88246
Alejandro Velez <alejovelez@nebular.co>
2022-11-06 11:02:04 -0500

    pages not found

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // Components
-import Navbar from './components/Navbar';
+
 import Home1 from './pages/Home1'
 // Routes
 import { Routes, Route } from 'react-router-dom';
 // Styles
 import './App.css';
+import './components/NotFound/NotFound.css'
 // Scroll To Top when change route
 import AutoToTop from './components/AutoToTop';
 // Components
 import Cities from './pages/Cities';
 import Hotels from './pages/Hotels';
 import Layout from './layout/Layout';
+import NotFoundPage from './pages/NotFoundPage';
+
 
 // Layout
 
@@ -19,23 +22,18 @@ import Layout from './layout/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <AutoToTop />
+      <Routes>
+        <Route path='/' exact element={<Home1 />}/>
+        <Route path='/cities' element={<Cities />}/>
+        <Route path='/hotels' element={<Hotels />}/>
+        <Route path='*' element={<NotFoundPage />}/>
+      </Routes>
+    </Layout>
+    
   );
 }
 
 export default App;
+
